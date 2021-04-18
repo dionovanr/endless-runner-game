8e1370bc27cbf9389835123dde79852c38f67e7d
kizaruu77 <dionovan7@gmail.com>
2021-04-18 11:09:31 +0700

    jumping

---
+++ 2D Endless Runner/Assets/Scripts/CharacterMoveController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveAccel;
+    public float maxSpeed;
+
+    [Header("Jump")]
+    public float jumpAccel;
+
+    [Header("Ground Raycast")]
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+
+    private bool isJumping;
+    private bool isOnGround;
+
+    private Rigidbody2D rb;
+
+  
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isOnGround)
+            {
+                isJumping = true;
+            }
+            
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundRaycastDistance, groundLayerMask);
+
+        if (hit)
+        {
+            if (!isOnGround && rb.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocityVector = rb.velocity;
+        if (isJumping)
+        {
+
+            velocityVector.y += jumpAccel;
+            isJumping = false;
+        }
+
+
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
+
+        rb.velocity = velocityVector;
+    }
+}
